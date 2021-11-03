295f291d72c520d022eb268960e488c3d7edb5c7
Jesus Ale <jesus.9314@gmail.com>
2021-11-03 00:11:16 -0500

    diseñando menú

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -1,4 +1,4 @@
-<header class="bg-trueGray-700">
+<header class="bg-trueGray-700 sticky top-0">
     <div class="container flex items-center h-16">
         <a class="flex flex-col items-center justify-center px-4 bg-white bg-opacity-25 text-white cursor-pointer font-semibold h-full">
             <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
@@ -10,5 +10,57 @@
             <x-jet-application-mark class="block h-9 w-auto"/>
         </a>
         @livewire('search')
+
+        <div class="mx-6 relative">
+            @auth
+            <x-jet-dropdown align="right" width="48">
+                <x-slot name="trigger">
+                        <button class="flex text-sm border-2 border-transparent rounded-full focus:outline-none focus:border-gray-300 transition">
+                            <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
+                        </button>
+                </x-slot>
+
+                <x-slot name="content">
+                    <!-- Account Management -->
+                    <div class="block px-4 py-2 text-xs text-gray-400">
+                        {{ __('Administrar Cuenta') }}
+                    </div>
+
+                    <x-jet-dropdown-link href="{{ route('profile.show') }}">
+                        {{ __('Perfil') }}
+                    </x-jet-dropdown-link>
+
+                    <div class="border-t border-gray-100"></div>
+
+                    <!-- Authentication -->
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+
+                        <x-jet-dropdown-link href="{{ route('logout') }}"
+                                 onclick="event.preventDefault();
+                                        this.closest('form').submit();">
+                            {{ __('Desconectarse') }}
+                        </x-jet-dropdown-link>
+                    </form>
+                </x-slot>
+            </x-jet-dropdown>
+            @else
+                <x-jet-dropdown align="right" width="48">
+                    <x-slot name="trigger">
+                        <i class="fas fa-user-circle text-white text-3xl cursor-pointer"></i>
+                    </x-slot>
+                    <x-slot name="content">
+                        <x-jet-dropdown-link href="{{ route('login') }}">
+                            {{ __('Login') }}
+                        </x-jet-dropdown-link>
+
+                        <x-jet-dropdown-link href="{{ route('register') }}">
+                            {{ __('Register') }}
+                        </x-jet-dropdown-link>
+                    </x-slot>
+                </x-jet-dropdown>
+            @endauth
+        </div>
+        @livewire('dropdown-cart')
     </div>
 </header>
