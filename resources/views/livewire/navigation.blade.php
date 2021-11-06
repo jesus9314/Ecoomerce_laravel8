de40aa7be423127bd28912de67b134d235bf4ee1
Jesus Ale <jesus.9314@gmail.com>
2021-11-05 21:38:19 -0500

    sliders de productos listo

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -1,17 +1,20 @@
-<header class="bg-trueGray-700 sticky top-0">
-    <div class="container flex items-center h-16">
-        <a class="flex flex-col items-center justify-center px-4 bg-white bg-opacity-25 text-white cursor-pointer font-semibold h-full">
+<header class="bg-trueGray-700 sticky top-0 z-50" x-data="dropdown()">
+    <div class="container flex items-center h-16 justify-between md:justify-start">
+        <a :class="{'bg-opacity-100 text-orange-500' : open}"
+        x-on:click="show()"
+        class="flex flex-col items-center justify-center order-last md:order-first px-6 md:px-4 bg-white bg-opacity-25 text-white cursor-pointer font-semibold h-full">
             <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
                 <path class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
             </svg>
-            <span>Categorías</span>
+            <span class="text-sm hidden md:block">Categorías</span>
         </a>
         <a href="/" class="mx-6">
             <x-jet-application-mark class="block h-9 w-auto"/>
         </a>
-        @livewire('search')
-
-        <div class="mx-6 relative">
+        <div class="flex-1 hidden md:block">
+            @livewire('search')
+        </div>
+        <div class="mx-6 relative hidden md:block">
             @auth
             <x-jet-dropdown align="right" width="48">
                 <x-slot name="trigger">
@@ -19,7 +22,6 @@
                             <img class="h-8 w-8 rounded-full object-cover" src="{{ Auth::user()->profile_photo_url }}" alt="{{ Auth::user()->name }}" />
                         </button>
                 </x-slot>
-
                 <x-slot name="content">
                     <!-- Account Management -->
                     <div class="block px-4 py-2 text-xs text-gray-400">
@@ -61,6 +63,91 @@
                 </x-jet-dropdown>
             @endauth
         </div>
-        @livewire('dropdown-cart')
+        <div class="hidden md:block">
+            @livewire('dropdown-cart')
+        </div>
     </div>
+    <nav id="navigation-menu"
+    x-show="open"
+    :class="{'block':open, 'hidden': !open}"
+    class="bg-trueGray-700 bg-opacity-25 w-full absolute hidden">
+     <!--Menú PC-->
+        <div class="container h-full hidden md:block">
+            <div
+            x-on:click.away="close()"
+            class="grid grid-cols-4 h-full relative">
+                <ul class="bg-white">
+                    @foreach ($categories as $category)
+                        <li class="navigation-link text-trueGray-500 hover:bg-orange-500 hover:text-white">
+                            <a href="" class="py-2 px-4 text-sm flex items-center">
+                                <span class="flex justify-center w-9">
+                                    {!!$category->icon!!}
+                                </span>
+                                {{$category->name}}
+                            </a>
+                            <div class="navigation-submenu bg-gray-100 absolute w-3/4 h-full top-0 right-0 hidden">
+                                <x-navigation-subcategories :category="$category"/>
+                            </div>
+                        </li>
+                    @endforeach
+                </ul>
+                <div class="col-span-3 bg-gray-100">
+                    <x-navigation-subcategories :category="$categories->first()"/>
+                </div>
+            </div>
+        </div>
+    <!--Menú Móvil-->
+        <div class="bg-white h-full overflow-y-auto">
+            <div class="container bg-gray-200 py-3 mb-2">
+                @livewire('search')
+            </div>
+            <ul>
+                @foreach ($categories as $category)
+                    <li class="text-trueGray-500 hover:bg-orange-500 hover:text-white">
+                        <a href="" class="py-2 px-4 text-sm flex items-center">
+                            <span class="flex justify-center w-9">
+                                {!!$category->icon!!}
+                            </span>
+                            {{$category->name}}
+                        </a>
+                    </li>
+                @endforeach
+            </ul>
+            <p class="text-trueGray-500 px-6 my-2">USUARIOS</p>
+            @livewire('cart-mobil')
+            @auth
+            <a href="{{ route('profile.show') }}" class="py-2 px-4 text-sm flex items-center text-trueGray-500 hover:bg-orange-500 hover:text-white">
+                <span class="flex justify-center w-9">
+                    <i class="far fa-address-card"></i>
+                </span>
+                Perfil
+            </a>
+            <a href=""
+            onclick="event.preventDefault();
+                    document.getElementById('logout-form').submit()"
+            class="py-2 px-4 text-sm flex items-center text-trueGray-500 hover:bg-orange-500 hover:text-white">
+                <span class="flex justify-center w-9">
+                    <i class="fas fa-sign-out-alt"></i>
+                </span>
+                Cerrar sesión
+            </a>
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
+                @csrf
+            </form>
+            @else
+            <a href="{{ route('login') }}" class="py-2 px-4 text-sm flex items-center text-trueGray-500 hover:bg-orange-500 hover:text-white">
+                <span class="flex justify-center w-9">
+                    <i class="fas fa-user-circle"></i>
+                </span>
+                Iniciar Sesión
+            </a>
+            <a href="{{ route('register') }}" class="py-2 px-4 text-sm flex items-center text-trueGray-500 hover:bg-orange-500 hover:text-white">
+                <span class="flex justify-center w-9">
+                    <i class="fas fa-fingerprint"></i>
+                </span>
+                Regístrate
+            </a>
+            @endauth
+        </div>
+    </nav>
 </header>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,2 +1,58 @@
 <x-app-layout>
+    <div class="container py-8">
+        @foreach ($categories as $category)
+            <section class="mb-6">
+                <h1 class="text-lg uppercase  font-semibold text-gray-700">
+                    {{$category->name}}
+                </h1>
+                @livewire('category-products', ['category' => $category])
+            </section>
+        @endforeach
+    </div>
+    @push('script')
+        <script>
+            Livewire.on('glider',function(id){
+                new Glider(document.querySelector('.glider-'+id), {
+                slidesToScroll: 1,
+                slidesToShow: 1,
+                draggable: true,
+                dots: '.glider-'+id+'~ .dots',
+                arrows: {
+                    prev: '.glider-'+id+'~ .glider-prev',
+                    next: '.glider-'+id+'~ .glider-next'
+                },
+                responsive: [
+                    {
+                        breakpoint: 640,
+                        settings:{
+                            slidesToScroll: 2,
+                            slidesToShow: 2.5,
+                        }
+                    },
+                    {
+                        breakpoint: 768,
+                        settings:{
+                            slidesToScroll: 3,
+                            slidesToShow: 3.5,
+                        }
+                    },
+                    {
+                        breakpoint: 1024,
+                        settings:{
+                            slidesToScroll: 4,
+                            slidesToShow: 4.5,
+                        }
+                    },
+                    {
+                        breakpoint: 1280,
+                        settings:{
+                            slidesToScroll: 5,
+                            slidesToShow: 5.5,
+                        }
+                    },
+                ]
+                });
+            });
+        </script>
+    @endpush
 </x-app-layout>
